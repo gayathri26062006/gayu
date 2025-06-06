cb58522839da8ccbe0634f25ed2d7a95010ba94f
gayathri26062006 <gayathriaarthi597@gmail.com>
2025-06-06 13:21:15 +0530

    Add files via upload

---
+++ gayu3.py
@@ -0,0 +1,9 @@
+a=15;
+b=10;
+print("Addition:",a+b)
+print("Subtraction:",a-b)
+print("Multiplication:",a*b)
+print("Division:",a/b)
+print("Floor division:",a//b)
+print("Modulus:",a%b)
+print("Exponent:",a**b)
